9ba6f555c29e03d0aeddd6919f43252e64c0a2a6
YoussefAswad55 <85849220+YoussefAswad55@users.noreply.github.com>
2021-11-10 21:53:26 +0200

    Added function to create pipes, also added timer to change location of pipes over time.

--- GraphicAssignment1.cpp
+++ GraphicAssignment1.cpp
@@ -7,58 +7,101 @@
 //  Methods Signatures
 void display();
 //void reshape(int , int);
-void timer();
+void timer(int);
 void drawRect(int x, int y, int w, int h);
 void drawCircle(int x, int y, float r);
+//-----------------
+
+//  Global Variables
+int t = 0;
+float playerX = 20;
+float playerY = 0;
+float xpos = 150;
+float pipeColor[3] = {0, 1, 0};
+float pipeColorAlt[3] = {1, 0, 0};
 //-----------------
 
 int main(int argc, char **argr)
 {
 	glutInit(&argc, argr);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(1200, 800);
+	glutInitWindowSize(1280, 720);
 	glutCreateWindow("Flappy Bird");
 
 	glutDisplayFunc(display);
+	glutTimerFunc(0, timer, 0);
 	//glutReshapeFunc(reshape)
 	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	gluOrtho2D(-150.0, 150.0, -100.0, 100.0);
+	gluOrtho2D(0, 300.0, -100.0, 100.0);
 
 	glutMainLoop();
 }
 
-void drawRect(int x, int y, int w, int h) {
-  glBegin(GL_POLYGON);
-  glVertex2f(x, y);
-  glVertex2f(x + w, y);
-  glVertex2f(x + w, y + h);
-  glVertex2f(x, y + h);
-  glEnd();
-}
-
-void drawCircle(int x, int y, float r) {
-  glPushMatrix();
-  glTranslatef(x, y, 0);
-  GLUquadric *quadObj = gluNewQuadric();
-  gluDisk(quadObj, 0, r, 50, 50);
-  glPopMatrix();
-}
-
-void timer()
+void drawRect(int x, int y, int w, int h, float color[3])
 {
+	glBegin(GL_POLYGON);
+	glColor3f(color[0], color[1], color[2]);
+	glVertex2f(x, y);
+	glVertex2f(x, y - h);
+	glVertex2f(x + w, y - h);
+	glVertex2f(x + w, y);
+	glEnd();
+}
 
+void drawCircle(int x, int y, float r)
+{
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	GLUquadric *quadObj = gluNewQuadric();
+	gluDisk(quadObj, 0, r, 50, 50);
+	glPopMatrix();
+}
+
+void drawPipeDown(int x, int h, float color[3])
+{
+	drawRect(x, -100 + h, 20, h, color);
+
+	drawRect(x - 3, -100 + 10 + h, 26, 10, color);
+}
+
+void drawPipeUp(int x, int h, float color[3])
+{
+	drawRect(x, 100, 20, h, color);
+
+	drawRect(x - 3, 100 - h, 26, 10, color);
+}
+
+void timer(int)
+{
+	glutPostRedisplay();
+	t += 1000 / 60;
+	glutTimerFunc(1000 / 60, timer, 0);
+
+	xpos -= 0.5;
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	if (xpos > 0)
+	{
+		glClearColor(1, 0, 0, 0);
+	}
+	else
+	{
+		glClearColor(0, 0, 1, 0);
+	}
+
+	drawCircle(playerX, playerY, 15);
+
 	//glLoadIdentity();
+	drawPipeDown(xpos, 40, pipeColor);
 
-
-	glFlush();
+	drawPipeUp(xpos, 40, pipeColor);
+	glutSwapBuffers();
 }
 
 /*
