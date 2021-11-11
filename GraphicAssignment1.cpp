5ababc853a4eaf41d82af678c5173871b47446db
YoussefAswad55 <85849220+YoussefAswad55@users.noreply.github.com>
2021-11-11 02:24:55 +0200

    Began work on the player model.
    TO-DO:
    
    1. Add acceleration for the fall down which breaks by pressing jump.
    2. Calculate score.
    3. Display Lives and Score.
    4. Collision with screen borders.
    5. Collision with pipes.
    6. Design powerup and display it.
    7. Collision with Powerup.
    8. Make sensible background colors.

--- GraphicAssignment1.cpp
+++ GraphicAssignment1.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argr)
 	glutMainLoop();
 }
 
-void drawRect(int x, int y, int w, int h, float color[3])
+void drawRect(float x, float y, float w, float h, float color[3])
 {
 	glBegin(GL_POLYGON);
 	glColor3f(color[0], color[1], color[2]);
@@ -57,23 +57,65 @@ void drawRect(int x, int y, int w, int h, float color[3])
 	glEnd();
 }
 
-void drawCircle(float x, float y, float r)
+void drawCircle(float x, float y, float r , float color[3])
 {
 	glPushMatrix();
+	glColor3f(color[0], color[1], color[2]);
 	glTranslatef(x, y, 0);
 	GLUquadric *quadObj = gluNewQuadric();
 	gluDisk(quadObj, 0, r, 50, 50);
 	glPopMatrix();
 }
+/*
+void drawPlayer()
+{
+	float r = 10;
+	float color[3] = {1,0.6,1};
+	drawCircle(playerX,playerY,r,color);
+	glBegin(GL_POLYGON);
+	glColor3f(color[0], color[1], color[2]);
+	glVertex2f(playerX+r-2,playerY+5);
+	glVertex2f(playerX+r-2, playerY-5);
+	glVertex2f(playerX+r+ 5, playerY);
+	glEnd();
 
-void drawPipeDown(int x, int h, float color[3])
+}
+*/
+
+
+void drawPlayer(){
+	int r = 69;
+	int g = 205;
+	int b = 62;
+	float color[3] = {0 , 0.3 , 0.7};
+	glBegin(GL_TRIANGLES);
+	glColor3f(0, 0.3, 0.6);
+	glVertex2f(playerX + 15 ,playerY + 15);
+	glVertex2f(playerX + 15 , playerY -15);
+	glVertex2d(playerX-15 , playerY+3);
+	glColor3f(1, 0.7, 0);
+	glVertex2f(playerX + 15 ,playerY + 15);
+	glVertex2f(playerX + 15 , playerY +5);
+	glVertex2d(playerX+ 25 , playerY+10);
+	glEnd();
+
+	glBegin(GL_LINES);
+	//glLineWidth(1000);
+	glColor3f(0, 0, 0);
+	glVertex2f(playerX + 15 ,playerY + 10);
+	glVertex2f(playerX + 25 , playerY +10);
+	glEnd();
+
+}
+
+void drawPipeDown(float x, float h, float color[3])
 {
 	drawRect(x, -180 + h, 40, h, color);
 
 	drawRect(x - 5, -180 + 15 + h, 50, 15, color);
 }
 
-void drawPipeUp(int x, int h, float color[3])
+void drawPipeUp(float x, float h, float color[3])
 {
 	drawRect(x, 180, 40, h, color);
 
@@ -82,8 +124,8 @@ void drawPipeUp(int x, int h, float color[3])
 
 void drawPipeDou(float x, float h, float color[3])
 {
-	drawPipeDown(x, h, pipeColor);
-	drawPipeUp(x, 280 - h, pipeColor);
+	drawPipeDown(x, h, color);
+	drawPipeUp(x, 280 - h, color);
 }
 
 void drawAllPipes()
@@ -151,14 +193,18 @@ void display()
 	glLoadIdentity();
 	if (xpos > -450)
 	{
-		glClearColor(1, 0, 0, 0);
+		glClearColor(0.7, 0, 0, 0);
 	}
-	else
+	else if(xpos > -950)
 	{
 		glClearColor(0, 0, 1, 0);
 	}
+	else{
+		glClearColor(1, 1, 1, 0);
+	}
+	
 	drawAllPipes();
 
-	drawCircle(playerX, playerY, 10);
+	drawPlayer();
 	glutSwapBuffers();
 }
