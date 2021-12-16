b5ec0874ff5afc9c7774af6ff39e8cd419251da0
YoussefAswad55 <85849220+YoussefAswad55@users.noreply.github.com>
2021-12-16 09:10:44 +0200

    Fixed fences and added changing colors to fences

--- GraphicsAssignment2.cpp
+++ GraphicsAssignment2.cpp
@@ -19,11 +19,13 @@ double torriGateScale = 1.1;
 double camX = 0;
 double camY = 0;
 double camZ = 0;
-bool collected[] = {false, false, false};
+bool collected[] = { false, false, false };
 int timer = 30;
+float fenceR = 0;
+float fenceG = 0;
+float fenceB = 0;
 
-void drawFloor()
-{
+void drawFloor() {
 	glPushMatrix();
 	glScaled(floorX, 0.02, floorZ);
 	glColor3d(0.0, 1.0, 0.0);
@@ -37,13 +39,10 @@ void drawTorus(int numc, int numt)
 	double s, t, x, y, z, twopi;
 
 	twopi = 2 * 3.14;
-	for (i = numc / 2; i < numc; i++)
-	{
+	for (i = numc / 2; i < numc; i++) {
 		glBegin(GL_QUAD_STRIP);
-		for (j = numt / 2; j <= numt; j++)
-		{
-			for (k = 1; k >= 0; k--)
-			{
+		for (j = numt / 2; j <= numt; j++) {
+			for (k = 1; k >= 0; k--) {
 				s = (i + k) % numc + 0.5;
 				t = j % numt;
 
@@ -57,8 +56,7 @@ void drawTorus(int numc, int numt)
 	}
 }
 
-void drawToriiGate(double scale)
-{
+void drawToriiGate(double scale) {
 
 	glPushMatrix();
 
@@ -70,7 +68,7 @@ void drawToriiGate(double scale)
 
 	glTranslated(-0.7, 2, 0);
 	glRotated(90, 1, 0, 0);
-	GLUquadricObj *qobj;
+	GLUquadricObj* qobj;
 	qobj = gluNewQuadric();
 	gluQuadricDrawStyle(qobj, GLU_LINE);
 	gluCylinder(qobj, 0.15, 0.15, 2, 800, 800);
@@ -96,10 +94,10 @@ void drawToriiGate(double scale)
 	glPopMatrix();
 
 	glPopMatrix();
+
 }
 
-void drawSanta(double x, double z, double ang)
-{
+void drawSanta(double x, double z, double ang) {
 
 	glPushMatrix();
 
@@ -113,7 +111,7 @@ void drawSanta(double x, double z, double ang)
 	glTranslated(-0.25, 1, 0);
 	glRotated(90, 1, 0, 0);
 	glColor3d(1, 0, 0);
-	GLUquadricObj *qobj;
+	GLUquadricObj* qobj;
 	qobj = gluNewQuadric();
 	gluQuadricDrawStyle(qobj, GLU_LINE);
 	gluCylinder(qobj, 0.1, 0.1, 1, 800, 800);
@@ -154,10 +152,11 @@ void drawSanta(double x, double z, double ang)
 	glutSolidCone(0.2, 0.5, 300, 300);
 	glPopMatrix();
 	glPopMatrix();
+
+
 }
 
-void drawTree(double x, double z, double scale)
-{
+void drawTree(double x, double z,double scale) {
 
 	glPushMatrix();
 	glTranslated(x, 0, z);
@@ -166,7 +165,7 @@ void drawTree(double x, double z, double scale)
 	glTranslated(0, 1, 0);
 	glRotated(90, 1, 0, 0);
 	glColor3f(0.7, 0, 0);
-	GLUquadricObj *qobj;
+	GLUquadricObj* qobj;
 	qobj = gluNewQuadric();
 	gluQuadricDrawStyle(qobj, GLU_LINE);
 	gluCylinder(qobj, 0.1, 0.1, 1, 800, 800);
@@ -194,10 +193,10 @@ void drawTree(double x, double z, double scale)
 	glPopMatrix();
 
 	glPopMatrix();
+
 }
 
-void drawPresent(double x, double z, double ang)
-{
+void drawPresent(double x, double z, double ang) {
 
 	glPushMatrix();
 	glTranslated(x, 0, z);
@@ -234,18 +233,17 @@ void drawPresent(double x, double z, double ang)
 	glPopMatrix();
 }
 
-void drawFence(double x, double z)
-{
+void drawFence(double x, double z) {
 	glPushMatrix();
-	glTranslated(x, 0, z);
-	glScaled(0.2, 2, 0.5);
-	glColor3d(1.0, 1.0, 0.0);
+	glTranslated(x, 0.5, z);
+	glScaled(0.2, 1, 0.5);
+	glColor3d(fenceR, fenceG, fenceB);
 	glutSolidCube(1.0);
 	glPopMatrix();
 }
-void drawSnowMan(double x, double z, double ang)
-{
+void drawSnowMan(double x, double z, double ang) {
 	glPushMatrix();
+
 
 	glTranslated(x, 0, z);
 
@@ -278,9 +276,9 @@ void drawSnowMan(double x, double z, double ang)
 	glutSolidCone(0.1, 0.2, 300, 300);
 	glPopMatrix();
 	glPopMatrix();
+
 }
-void drawFenceRotateX(double x, double z)
-{
+void drawFenceRotateX(double x, double z) {
 	glPushMatrix();
 	glTranslated(x, 0.5, z);
 	glRotated(90, 1, 0, 0);
@@ -290,99 +288,92 @@ void drawFenceRotateX(double x, double z)
 	glPopMatrix();
 }
 
-void drawFences()
-{
-	drawFence((floorX / 2.0f) - 0.1, -((floorZ / 2.0f) - 0.25));
-	drawFence(-(floorX / 2.0f) + 0.1, -((floorZ / 2.0f) - 0.25));
+void drawFences() {
 
-	drawFenceRotateX((floorX / 2.0f) - 0.1, (floorZ / 3.0f));
+	float p = -5;
+	float inc = 1.4;
+	
+	for (int i = 0; i < 6; i++) {
+		
+		p += inc;
+		drawFence(3.5, p);
+	}
+	p = -5;
+	for (int i = 0; i < 6; i++) {
 
-	drawFence((floorX / 2.0f) - 0.1, -(floorZ / 4.0f));
-	drawFence(-(floorX / 2.0f) + 0.1, -(floorZ / 4.0f));
-
-	drawFence((floorX / 2.0f) - 0.1, 0);
-	drawFence(-(floorX / 2.0f) + 0.1, 0);
-
-	drawFence((floorX / 2.0f) - 0.1, (floorZ / 4.0f));
-	drawFence(-(floorX / 2.0f) + 0.1, (floorZ / 4.0f));
-
-	drawFence((floorX / 2.0f) - 0.1, (floorZ / 2.0f) - 0.25);
-	drawFence(-(floorX / 2.0f) + 0.1, (floorZ / 2.0f) - 0.25);
+		p += inc;
+		drawFence(-3.5, p);
+	}
+	inc = 1;
+	p = -3.5;
+	for (int i = 0; i < 6; i++) {
+		p += inc;
+		drawFence(p, 4);
+	}
+	
 }
 
-void AnimPresent()
-{
+
+void AnimPresent() {
 	presentAng += 5;
 
 	glutPostRedisplay();
 }
 
-void AnimSnowMan()
-{
+void AnimSnowMan() {
 	snowManAng -= 4;
 
 	glutPostRedisplay();
 }
 
-void AnimTree()
-{
-	if (treeScale >= 0.5 && treeScale < 1)
-	{
+void AnimTree() {
+	if (treeScale >= 0.5 && treeScale < 1) {
 		treeScale += 0.1;
 	}
-	else if (treeScale <= 1 && treeScale > 0.5)
-	{
+	else if (treeScale <= 1 && treeScale > 0.5) {
 		treeScale -= 0.1;
 	}
 
 	glutPostRedisplay();
 }
 
-void AnimGate()
-{
+void AnimGate() {
 
-	if (torriGateScale >= 0.5 && torriGateScale < 1.1)
-	{
+
+	if (torriGateScale >= 0.5 && torriGateScale < 1.1) {
 		torriGateScale += 0.1;
 	}
-	else if (torriGateScale <= 1.1 && torriGateScale > 0.5)
-	{
+	else if (torriGateScale <= 1.1 && torriGateScale > 0.5) {
 		torriGateScale -= 0.1;
 	}
 
 	glutPostRedisplay();
 }
 
-void Keyboard(unsigned char key, int x, int y)
-{
+void Keyboard(unsigned char key, int x, int y) {
 	float d = 0.1;
 
-	switch (key)
-	{
+	switch (key) {
 	case 'w':
-		if ((playerZ - 0.15) >= -(floorZ / 2) + 0.3)
-		{
+		if ((playerZ - 0.15) >= -(floorZ / 2) + 0.3) {
 			playerZ -= 0.5;
 			playerAng = 180;
 		}
 		break;
 	case 's':
-		if ((playerZ + 0.15) <= +(floorZ / 2) - 0.3)
-		{
+		if ((playerZ + 0.15) <= +(floorZ / 2) - 0.3) {
 			playerZ += 0.5;
 			playerAng = 0;
 		}
 		break;
 	case 'a':
-		if ((playerX - 0.15) >= -(floorX / 2) + 0.5)
-		{
+		if ((playerX - 0.15) >= -(floorX/2)+0.5) {
 			playerX -= 0.5;
 			playerAng = -90;
 		}
 		break;
 	case 'd':
-		if ((playerX + 0.15) <= +(floorX / 2) - 0.5)
-		{
+		if ((playerX + 0.15) <= +(floorX / 2) - 0.5) {
 			playerX += 0.5;
 			playerAng = 90;
 		}
@@ -414,7 +405,7 @@ void Keyboard(unsigned char key, int x, int y)
 	case 'z':
 		camZ -= 0.5;
 		break;
-	case 'x':
+	case 'x' :
 		camZ += 0.5;
 		break;
 	case '1':
@@ -441,26 +432,20 @@ void Keyboard(unsigned char key, int x, int y)
 	drawPresent(2.5, -3, presentAng);
 	*/
 
-	if (playerX < (-2 + 0.35) && playerX > (-2 - 0.35))
-	{
-		if (playerZ < (-3 + 0.35) && playerZ > (-3 - 0.35))
-		{
+	if (playerX  < (-2 + 0.35) && playerX  > (-2 - 0.35) ) {
+		if (playerZ  < (-3 + 0.35) && playerZ  >(-3 - 0.35)) {
 			collected[0] = true;
 		}
 	}
 
-	if (playerX < (0 + 0.35) && playerX > (0 - 0.35))
-	{
-		if (playerZ < (3 + 0.35) && playerZ > (3 - 0.35))
-		{
+	if (playerX  < (0 + 0.35) && playerX  >(0 - 0.35)) {
+		if (playerZ  < (3 + 0.35) && playerZ  >(3 - 0.35)) {
 			collected[1] = true;
 		}
 	}
 
-	if (playerX < (2.5 + 0.35) && playerX > (2.5 - 0.35))
-	{
-		if (playerZ < (-3 + 0.35) && playerZ > (-3 - 0.35))
-		{
+	if (playerX  < (2.5 + 0.35) && playerX  >(2.5 - 0.35)) {
+		if (playerZ  < (-3 + 0.35) && playerZ  >(-3 - 0.35)) {
 			collected[2] = true;
 		}
 	}
@@ -486,16 +471,21 @@ void drawText(int x, int y, string s)
 	}
 }
 
-void Timer(int)
-{
+
+
+
+
+void Timer(int) {
 	glutPostRedisplay();
 	timer -= 1;
-	printf(timer + "");
-	glutTimerFunc(1000, Timer, 0);
+	printf(timer+"");
+	fenceR += 0.1;
+	fenceB += 0.2;
+	fenceG += 0.05;
+		glutTimerFunc(1000, Timer, 0);
 }
 
-void setupCamera()
-{
+void setupCamera() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	//glOrtho(-0.5, 0.5, -0.5, 0.5, -1, 1);
@@ -503,26 +493,22 @@ void setupCamera()
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0.0 + camX, 5.0 + camY, 8.0 + camZ, 0.0, 0.0, 0.0, 0, 2.0, 0);
+	gluLookAt(0.0+camX, 5.0+camY, 8.0+camZ, 0.0, 0.0, 0.0, 0, 2.0, 0);
 }
 
-void Display(void)
-{
+void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	setupCamera();
 	drawFences();
 	drawSnowMan(2.5, 3, snowManAng);
-	if (!collected[0])
-	{
+	if (!collected[0]) {
 		drawPresent(-2, -3, presentAng);
 	}
 
-	if (!collected[1])
-	{
+	if (!collected[1]) {
 		drawPresent(0, 3, presentAng);
 	}
-	if (!collected[2])
-	{
+	if (!collected[2]) {
 		drawPresent(2.5, -3, presentAng);
 	}
 	drawToriiGate(torriGateScale);
@@ -530,14 +516,13 @@ void Display(void)
 	drawTree(-2, 2.5, treeScale);
 	drawSanta(playerX, playerZ, playerAng);
 
-	if (collected[0] && collected[1] && collected[2])
-	{
+
+	if (collected[0] && collected[1] && collected[2]) {
 
 		drawText(0, 0, "You Won!");
 	}
 
-	else if (timer <= 0)
-	{
+	else if (timer <= 0) {
 		drawText(0, 0, "You Lost!");
 	}
 
@@ -547,13 +532,12 @@ void Display(void)
 	glFlush();
 }
 
-void Special(int key, int x, int y)
-{
 
-	switch (key)
-	{
+void Special(int key, int x, int y) {
+
+	switch (key) {
 	case GLUT_KEY_UP:
-		camY += 0.5;
+		camY+= 0.5;
 		break;
 	case GLUT_KEY_DOWN:
 		camY -= 0.5;
@@ -568,15 +552,18 @@ void Special(int key, int x, int y)
 		break;
 	}
 
+
 	glutPostRedisplay();
 }
 
-int main(int argc, char **argv)
-{
+
+
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 
 	glutInitWindowSize(1280, 720);
 	glutInitWindowPosition(150, 150);
+
 
 	glutCreateWindow("OpenGL - 3D Template");
 	glutDisplayFunc(Display);
@@ -587,6 +574,7 @@ int main(int argc, char **argv)
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
 	glEnable(GL_DEPTH_TEST);
+	
 
 	glutTimerFunc(0, Timer, 0);
 
